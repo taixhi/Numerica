ece1ddeec35dfb585f28f0ac260dd8e6e65819ec
Taichi Kato <taichikato0909@gmail.com>
2017-06-01 19:00:17 +0800

    Initial Commit

---
+++ NumericaTests/NumericaTests.swift
@@ -0,0 +1,36 @@
+//
+//  NumericaTests.swift
+//  NumericaTests
+//
+//  Created by Taichi Kato on 1/6/17.
+//  Copyright © 2017 taichikato. All rights reserved.
+//
+
+import XCTest
+@testable import Numerica
+
+class NumericaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
